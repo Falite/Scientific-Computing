35bb2b6f8237ca4b370e595e49260c8b3dcf4bd3
Falite <abdoullatifomarjee@gmail.com>
2025-02-06 11:00:10 +0400

    Initial Commit

---
+++ Scientific-Computing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Scientific-Computing
+//
+//  Created by Abdoullatif.O pro  on 06/02/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
