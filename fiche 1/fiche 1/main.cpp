ee4d5ccafa3a92bb50e8d934c07791c02a639de0
Falite <abdoullatifomarjee@gmail.com>
2025-02-15 01:31:21 +0400

    initialisation fiche1

---
+++ fiche 1/fiche 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fiche 1
+//
+//  Created by Abdoullatif.O pro  on 14/02/2025.
+//
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
